335f11794fd5835a5e9892611efca0b2028cf66a
ddolsen <32841888+ddolsen@users.noreply.github.com>
2021-08-29 22:55:29 -0500

    Create check-if-string-ends-with.js

---
+++ algorithms/check-if-string-ends-with.js
@@ -0,0 +1,28 @@
+// Check if a string (first argument, str) ends with the given target string (second argument, target)
+
+// =======
+// == 1 ==
+// =======
+function confirmEnding(str, target) {
+    return str.slice(str.length - target.length) === target;
+}
+confirmEnding("foo bar", "bar");
+
+
+// =======
+// == 2 ==
+// =======
+function confirmEnding(str, target) {
+    let re = new RegExp(target + "$", "i");
+    return re.test(str);
+}
+console.log(confirmEnding("foo bar", "bar"));
+
+
+// =======
+// == 3 ==
+// =======
+function confirmEnding(str, target) {
+    return str.slice(-target.length) === target;
+}
+confirmEnding("Bastian", "n");
