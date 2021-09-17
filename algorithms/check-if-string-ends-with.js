d0da961bdf9b879dc37f80d0212eec4bcd33ed61
ddolsen <devin.d.olsen@gmail.com>
2021-09-16 20:21:47 -0500

    Update factorialize-a-number.js & check-if-string-ends-with.js

--- algorithms/check-if-string-ends-with.js
+++ algorithms/check-if-string-ends-with.js
@@ -1,27 +1,31 @@
 // Check if a string (first argument, str) ends with the given target string (second argument, target)
 
-// ================
-// == Solution 1 ==
-// ================
+/*
+================
+== Solution 1 ==
+================
+*/
 function confirmEnding(str, target) {
     return str.slice(str.length - target.length) === target;
 }
 console.log(confirmEnding("foo bar", "bar"));
 
-
-// ================
-// == Solution 2 ==
-// ================
+/*
+================
+== Solution 2 ==
+================
+*/
 function confirmEnding(str, target) {
     let re = new RegExp(target + "$", "i");
     return re.test(str);
 }
 console.log(confirmEnding("foo bar", "bar"));
 
-
-// ================
-// == Solution 3 ==
-// ================
+/*
+================
+== Solution 3 ==
+================
+*/
 function confirmEnding(str, target) {
     return str.slice(-target.length) === target;
 }
--- algorithms/factorialize-a-number.js
+++ algorithms/factorialize-a-number.js
@@ -13,3 +13,59 @@ function factorialize(num) {
     return product;
 }
 console.log(factorialize(5));
+
+/*
+================
+== Solution 2 ==
+================
+- The termination condition (AKA, base case) is the condition upon which a recursive function/solution stops recurring. It is the problem in a recursive function that we know how to solve explicitly; the "small" problem to which we know the answer.
+- If num == 0, then we return 1, effectively ending the recursion and informing the stack to propagate this value to the upper levels
+- If we do not have this condition, the recursion would go on until the stack space gets consumed, thereby resulting in a stack overflow
+*/
+function factorialize(num) {
+    if (num === 0) {
+        return 1;
+    }
+    return num * factorialize(num - 1);
+}
+console.log(factorialize(5));
+
+/*
+================
+== Solution 3 ==
+================
+- Tail recursion is used to optimize the the memory use
+- In traditional head recursion, the typical model is that you perform your recursive calls first, and then you take the return value of the recursive call and calculate the result. In this manner, you don’t get the result of your calculation until you have returned from every recursive call.
+- In tail recursion, you perform your calculations first, and then you execute the recursive call, passing the results of your current step to the next recursive step. This results in the last statement being in the form of (return (recursive-function params)).
+- In this solution, with each evaluation of the recursive call, the factorial is updated. This is different from the head-recursive solution where all evaluation calculations are stored on the stack until the base case is reached.
+*/
+function factorialize(num, factorial = 1) {
+    if (num == 0) {
+        return factorial;
+    } else {
+        return factorialize(num - 1, factorial * num);
+    }
+}
+
+console.log(factorialize(5));
+
+/*
+================
+== Solution 4 ==
+================
+
+- The “reduce” function is used to find the factorial value of the number
+- An array is created which has length num, and all elements of the array are filled as undefined (because empty arrays are not reducible). The array can be filled any number of different ways.
+- The reduce function’s accumulator is calling product, which is also our final value. The index value is multiplied by/with the product to find factorial value.
+- The product’s initial value is set to 1 because if it was zero the product would always be zero
+- Also, the factorial value cannot calculate negative numbers (this is tested)
+
+*/
+function factorialize(num, factorial = 1) {
+    return num < 0 ? 1 : (
+        new Array(num)
+            .fill(undefined)
+            .reduce((product, val, index) => product * (index + 1), 1)
+    );
+}
+console.log(factorialize(5));
