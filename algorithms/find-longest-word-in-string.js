397577af3bffc6240d761afdd79c6632d99523ad
ddolsen <32841888+ddolsen@users.noreply.github.com>
2021-09-21 21:54:01 -0500

    Update find-longest-word-in-string.js

--- algorithms/find-longest-word-in-string.js
+++ algorithms/find-longest-word-in-string.js
@@ -8,3 +8,11 @@ function findLongestWordLength(str) {
     }
     return maxLength;
 }
+
+
+function findLongestWordLength(s) {
+    return s.split(' ')
+        .reduce(function (longest, word) {
+            return Math.max(longest, word.length)
+        }, 0);
+}
